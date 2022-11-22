c664af02376879f460e7284a83641aa5117323a3
kimspractice <tpghksdldia@naver.com>
2022-11-22 15:08:14 +0900

    Saving results

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ from requests import get
 from bs4 import BeautifulSoup
 
 BASE_URL = "https://weworkremotely.com/remote-jobs/search?utf8=✓&term="
-SEARCH = "python"
-
+SEARCH = "react"
 
 response = get(f"{BASE_URL}{SEARCH}")
 
 if response.status_code != 200:
-   print("Can't request website")
+  print("Can't request website")
 else:
-   soup = BeautifulSoup(response.text,'html.parser')
-   jobs = soup.find_all('section',class_='jobs')
-   for job_section in jobs:
-     job_posts = job_section.find_all("li")
-     job_posts.pop(-1)
-     for post in job_posts:
-       anchors = post.find_all('a')
-       anchor = anchors[1]
-       link = anchor['href']
-       company,kind,region = anchor.find_all('span',class_='company')
-       title = anchor.find('span',class_='title')
-       print(company,kind,region,title)
-       print('--------------------------------------------')
+  results = []
+  soup = BeautifulSoup(response.text, 'html.parser')
+  jobs = soup.find_all('section', class_='jobs')
+  for job_section in jobs:
+    job_posts = job_section.find_all("li")
+    job_posts.pop(-1)
+    for post in job_posts:
+      anchors = post.find_all('a')
+      anchor = anchors[1]
+      link = anchor['href']
+      company, kind, region = anchor.find_all('span', class_='company')
+      title = anchor.find('span', class_='title')
+
+      job_data = {
+        'company': company.string,
+        'region': region.string,
+        'position': title.string
+      }
+      results.append(job_data)
+
+  for result in results:
+    print(result)
+    print("//////////////////////////////////")
