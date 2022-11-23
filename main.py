c190d4eb440494a47b5380d7171cfe4b92699d0c
kimspractice <tpghksdldia@naver.com>
2022-11-23 14:22:55 +0900

    indeed 403 fix

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.weworkremotely import extract_weworkremotely_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_weworkremotely_jobs("python")
-print(jobs)
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+browser = webdriver.Chrome(options=options)
+
+browser.get(f"{base_url}{search_term}")
+
+print(browser.page_source)
